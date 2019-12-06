4a7b1540110c8886a71229619e8a3d0dfeb0f5bd
Saad <s.alajlan10@gmail.com>
2019-12-06 16:16:53 +0300

    Initial Commit

---
+++ Virtual-Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual-Tourist
+//
+//  Created by Saad on 12/6/19.
+//  Copyright © 2019 saad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
