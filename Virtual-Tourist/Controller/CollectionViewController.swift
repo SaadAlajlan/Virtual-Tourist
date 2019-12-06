5062fdc2ad8cf94a5271e0d79b0769f9db8d8c13
Saad <s.alajlan10@gmail.com>
2019-12-06 16:50:04 +0300

    Project structure

---
+++ Virtual-Tourist/Controller/CollectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewController.swift
+//  Virtual-Tourist
+//
+//  Created by Saad on 12/6/19.
+//  Copyright © 2019 saad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController : UIViewController{
+    
+
+    
+}
+extension CollectionViewController : UICollectionViewDelegate{
+    
+}
